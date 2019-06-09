e6bc5a27875004ca594962f6d6fef042190159e5
taegun <taegun89@gmail.com>
2019-06-09 18:11:51 +0900

    test2

--- src/git_test_chobby/testing.java
+++ src/git_test_chobby/testing.java
@@ -3,7 +3,6 @@ package git_test_chobby;
 public class testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hihi");
-		System.out.println("222");
+		System.out.println("change");
 	}
 }
