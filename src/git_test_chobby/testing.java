2e78c6a6036990d371558b339023ad0f818d1597
zzcho <chobby94@naver.com>
2019-06-09 18:10:30 +0900

    hihi

---
+++ src/git_test_chobby/testing.java
@@ -0,0 +1,9 @@
+package git_test_chobby;
+
+public class testing {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hihi");
+		System.out.println("222");
+	}
+}
